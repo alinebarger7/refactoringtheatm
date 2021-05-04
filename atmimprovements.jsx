ae636e39a18521dfdcd4d564ff666c81dfd002d3
alinebarger7 <76960283+alinebarger7@users.noreply.github.com>
2021-05-04 13:54:27 -0400

    Update atmimprovements.jsx

--- atmimprovements.jsx
+++ atmimprovements.jsx
@@ -2,7 +2,7 @@ const ATMDeposit = ({ onChange, isDeposit, isValid }) => {
   const choice = ['Deposit', 'Cash Back'];
   console.log(`ATM isDeposit: ${isDeposit}`);
   return (
-    <label className="label huge">
+    <label className="label huge" className="moneyInput">
       <h3> {choice[Number(!isDeposit)]}</h3>
       <input id="number-input" type="number" width="200" onChange={onChange}></input>
       <input type="submit" disabled={!isValid} width="200" value="Submit" id="submit-input"></input>
@@ -11,25 +11,24 @@ const ATMDeposit = ({ onChange, isDeposit, isValid }) => {
 };
 
 const Account = () => {
-  // let deposit = 0; // state of this transaction
   const [deposit, setDeposit] = React.useState(0);
   const [totalState, setTotalState] = React.useState(0);
   const [isDeposit, setIsDeposit] = React.useState(true);
-  const [atmMode, setAtmMode] = React.useState('');
+  const [atmMode, setAtmMode] = React.useState("");
   const [validTransaction, setValidTransaction] = React.useState(false);
 
   let status = `Account Balance $ ${totalState} `;
   console.log(`Account Rendered with isDeposit: ${isDeposit}`);
   const handleChange = (event) => {
     console.log(Number(event.target.value));
-    if (Number(event.target.value) <= 0) {
-      return setValidTransaction(false);
-    }
-    if (atmMode === 'Cash Back' && Number(event.target.value) > totalState) {
-      setValidTransaction(false);
-    } else {
-      setValidTransaction(true);
-    }
+    if (Number(event.target.value) < 0) {
+      return setValidTransaction(false)
+    };
+    if (atmMode == "Cash Back" && Number(event.target.value) >= totalState) {
+      setValidTransaction(false)
+    }else{
+      setValidTransaction(true)
+    };
     setDeposit(Number(event.target.value));
   };
   const handleSubmit = (event) => {
@@ -38,40 +37,31 @@ const Account = () => {
     setValidTransaction(false);
     event.preventDefault();
   };
-
   const handleModeSelect = (event) => {
-    console.log(event.target.value);
     setAtmMode(event.target.value);
-    setValidTransaction(false);
-    if (event.target.value === 'Deposit') {
+    setValidTransaction(false)
+    if (event.target.value == "Deposit"){
       setIsDeposit(true);
-    } else {
+    };
+    if (event.target.value == "Cash Back"){
       setIsDeposit(false);
-    }
+    };
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <>
-        <h2 id="total">{status}</h2>
-        <label>Select an action below to continue</label>
+      <h2 className="userInput" id="total">{status}</h2>
+      <label>Select an action below to continue</label>
+      <div align="center">
         <select onChange={(e) => handleModeSelect(e)} name="mode" id="mode-select">
           <option id="no-selection" value=""></option>
-          <option id="deposit-selection" value="Deposit">
-            Deposit
-          </option>
-          <option id="cashback-selection" value="Cash Back">
-            Cash Back
-          </option>
+          <option id="deposit-selection" value="Deposit">Deposit</option>
+          <option id="cashback-selection" value="Cash Back">Cash Back</option>
         </select>
-        {atmMode && (
-          <ATMDeposit
-            onChange={handleChange}
-            isDeposit={isDeposit}
-            isValid={validTransaction}
-          ></ATMDeposit>
-        )}
-      </>
+      </div>
+      {
+        atmMode && <ATMDeposit onChange={handleChange} isDeposit={isDeposit} isValid={validTransaction}></ATMDeposit>
+      }
     </form>
   );
 };
